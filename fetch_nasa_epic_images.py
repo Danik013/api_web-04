79e8996083a1d27e2616221c7cb7dc3c86a9d778
Danik013 <2077647@gmail.com>
2025-04-09 16:11:00 +0300

    moved the params to the function download_image()

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -2,7 +2,6 @@ import requests
 import os
 from datetime import datetime
 from dotenv import load_dotenv
-from utils_functions import download_image
 from urllib.parse import urlencode
 
 
@@ -11,20 +10,27 @@ def fetch_nasa_epic_links(token):
     epic_payload = {"api_key": token}
     response = requests.get(nasa_epic_api_url, params=epic_payload)
     response.raise_for_status()
+    
     photo_links = []
 
     for photo_elements in response.json():
         image_id = photo_elements["image"]
         date = datetime.strptime(photo_elements["date"], "%Y-%m-%d %H:%M:%S")
         base_url = "https://api.nasa.gov/EPIC/archive/natural"
-        photo_elements_payload = {"api_key": token}
-        photo_elements_url = f"{base_url}/{date.strftime('%Y/%m/%d')}/png/{image_id}.png?{urlencode(photo_elements_payload)}"
-        photo_links.append(photo_elements_url)
+        photo_links.append(f"{base_url}/{date.strftime('%Y/%m/%d')}/png/{image_id}.png")
 
     for index, url in enumerate(photo_links, start=1):
         filename = f"nasa_epic_{index}.png"
         file_path = os.path.join("images", filename)
-        download_image(url, file_path)
+        download_image(url, file_path, epic_payload)
+
+
+def download_image(url, file_path, epic_payload):
+    response = requests.get(url, params=epic_payload)
+    response.raise_for_status()
+    with open(file_path, "wb") as file:
+        file.write(response.content)
+    return
 
 
 def main():
